7b833893b4175f642fd719f2139e444b9a25c51e
Mads <YOUR EMAIL ADDRESS>
2016-02-24 10:55:11 +0100

    Vi er de beste, spesielt ketil sin losning

---
+++ Oving8/exercise8.adb
@@ -0,0 +1,140 @@
+with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Numerics.Float_Random;
+use  Ada.Text_IO, Ada.Integer_Text_IO, Ada.Numerics.Float_Random;
+
+procedure exercise8 is
+
+    Count_Failed    : exception;    -- Exception to be raised when counting fails
+    Gen             : Generator;    -- Random number generator
+
+    protected type Transaction_Manager (N : Positive) is
+        entry Finished;
+		entry Wait_Until_Aborted;
+        function Commit return Boolean;
+        procedure Signal_Abort;
+    private
+        Finished_Gate_Open  : Boolean := False;
+		Abort_Gate_Open		: Boolean := False;
+        Aborted             : Boolean := False;
+    end Transaction_Manager;
+    protected body Transaction_Manager is
+        entry Finished when Finished_Gate_Open or Finished'Count = N is
+        begin
+            ------------------------------------------
+            -- PART 3: Complete the exit protocol here
+            ------------------------------------------
+			if Finished'Count = N-1 then
+				Finished_Gate_Open := True;
+			elsif Finished'Count = 0 then
+				Finished_Gate_Open := False;
+			end if;
+
+        end Finished;
+		
+		entry Wait_Until_Aborted when (Aborted and Wait_Until_Aborted'Count = N) or Abort_Gate_Open is
+		begin
+			if Wait_Until_Aborted'Count = N-1 then
+				Abort_Gate_open := True;
+			elsif Wait_Until_Aborted'Count = 0 then
+				Abort_Gate_open := False;
+				Aborted := False;
+			end if;
+		end Wait_Until_Aborted;
+
+        procedure Signal_Abort is
+        begin
+            Aborted := True;
+        end Signal_Abort;
+
+        function Commit return Boolean is
+        begin
+            --return Should_Commit;
+			return True;
+        end Commit;
+        
+    end Transaction_Manager;
+
+
+
+    
+    function Unreliable_Slow_Add (x : Integer) return Integer is
+    Error_Rate : Constant := 0.15;  -- (between 0 and 1)
+	Rand_Float : Standard.Float := 0.0;
+    begin
+        -------------------------------------------
+        -- PART 1: Create the transaction work here
+        -------------------------------------------'
+		Rand_Float:= Random(Gen);
+		if Rand_Float > Error_Rate then
+			delay Duration(0.1+Random(gen));
+			return x+10;
+		else
+			raise Count_Failed;
+		end if;
+    end Unreliable_Slow_Add;
+
+	function Reliable_Fast_Add(x : Integer) return Integer is
+	begin
+		return x + 5;
+	end Reliable_Fast_Add;
+
+
+
+
+    task type Transaction_Worker (Initial : Integer; Manager : access Transaction_Manager);
+    task body Transaction_Worker is
+        Num         : Integer   := 0;
+        Prev        : Integer   := Num;
+        Round_Num   : Integer   := 0;
+    begin
+        Put_Line ("Worker" & Integer'Image(Initial) & " started");
+
+        loop
+            Put_Line ("Worker" & Integer'Image(Initial) & " started round" & Integer'Image(Round_Num));
+            Round_Num := Round_Num + 1;
+
+            ---------------------------------------
+            -- PART 2: Do the transaction work here             
+            ---------------------------------------
+
+			select
+				Manager.Wait_Until_Aborted;
+				Num := Reliable_Fast_Add(Prev);
+			then abort
+				begin
+					Num := Unreliable_Slow_Add(Prev);
+				exception
+					when Count_Failed =>
+					Manager.Signal_Abort;
+				end;
+	            Manager.Finished;
+			end select;
+
+
+
+            if Manager.Commit = True then
+                Put_Line ("  Worker" & Integer'Image(Initial) & " comitting" & Integer'Image(Num));
+            else
+                Put_Line ("  Worker" & Integer'Image(Initial) &
+                             " reverting from" & Integer'Image(Num) &
+                             " to" & Integer'Image(Prev));
+                -------------------------------------------
+                -- PART 2: Roll back to previous value here
+                -------------------------------------------
+				Num := Prev;
+            end if;
+
+            Prev := Num;
+            delay 0.5;
+
+        end loop;
+    end Transaction_Worker;
+
+    Manager : aliased Transaction_Manager (3);
+
+    Worker_1 : Transaction_Worker (0, Manager'Access);
+    Worker_2 : Transaction_Worker (1, Manager'Access);
+    Worker_3 : Transaction_Worker (2, Manager'Access);
+
+begin
+    Reset(Gen); -- Seed the random number generator
+end exercise8;
