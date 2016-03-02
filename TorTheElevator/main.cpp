c51ffa708aa363e76fae3e03fc70060c90a7a023
Sverre Velten Rothmund <sverre_vr@hotmail.com>
2016-03-02 10:55:21 +0100

    Forbedret litt

--- TorTheElevator/main.cpp
+++ TorTheElevator/main.cpp
@@ -1,5 +1,5 @@
 #include <thread>
-#include <mutex>
+#include <atomic>
 #include <time.h>
 #include <comedi.h>
 #include "main_include.h"
@@ -11,26 +11,15 @@ using namespace std;
 
 
 int main(){
-	UDP messageInn();
-	messageInn.msg(flor, button, price, sendtype);
-
-
-
+	NetworkMessage network(/*Her må vi ha med porter, ipadresse og alt som hindrer oss fra å ha flere instanser*/);
+	
 	bool requestMatrix[N_FLOORS][REQUEST_MATRIX_WIDTH];
-	int finishedFloor;
-	mutex finishedFloorMutex;
-	thread state_machine(requestMatrix,finishedFloor,finishedRequestMutex);
+	atomic<int> finishedFloor;
+	thread state_machine(requestMatrix,finishedFloor);
 
 	bool buttonPressMatrix[N_FLOORS][N_BUTTONS];
 
 	time_t requestTimeoutMatrix[N_FLOORS];
-
-	message msgInn;
-	mutex msgInnMutex;
-	bool newMessage;
-	thread network_reciever(&msgInn, &msgInnMutex, &newMessage);
-
-	message msgOut;
 
 	int tempCost;
 	while(1){
@@ -39,31 +28,26 @@ int main(){
 		//If a button that has not been requested is pressed, then do request
 		for(int floor = 0; floor<N_FLOORS; ++floor){
 			for (int button = 0; button < N_BUTTONS; ++button) {
-				if(buttonPressMatrix[floor][button] && !requestMatrix[floor][button]){
-					handle_request(requestMatrix, floor, button, -1, requestTimeoutMatrix);
+				if(buttonPressMatrix[floor][button] && !requestMatrix[floor][button]){
+					handle_request(requestMatrix, floor, button, -1, requestTimeoutMatrix, &network);
 				}
 			}
 		}
 
 		//if we recieve a message
-		if(newMessage){
-			newMessage = 0;
-			msgInnMutex.lock;
-			//We recieved a request
-			if(msgInn.messageType == messageRequest){
-				handle_request(requestMatrix, msgInn.floor, msgInn.button, msgInn.price, requestTimeoutMatrix);
+		if(network.receiveMsg()){
+			if(network.receiveMsg.messageType == messageRequest){
+				handle_request(requestMatrix, network.receiveMsg.floor, network.receiveMsg.button, network.receiveMsg.price, requestTimeoutMatrix);
 			}
 			//We recieved a visited floor.
-			else if(msgInn.messageType == messageComplete){
-				clear_request(requestMatrix,floor,0,requestTimeoutMatrix);
+			else if(network.receiveMsg.messageType == messageComplete){
+				clear_request(requestMatrix,network.receiveMsg.floor,network.receiveMsg.button,requestTimeoutMatrix);
 			}
-			
-			msgInnMutex.unlock;
 		}
 
 		//If a request has timed out, then re-request
 		for(int floor=0; floor<N_FLOORS; ++floor){
-			if(requestTimeoutMatrix[floor] > time(NOW)){
+			if(requestTimeoutMatrix[floor] > time(NULL)){
 				//Arbitrarily gives priority on button up, will this be a problem?
 				if(requestMatrix[floor][UP_BUTTON]){
 					handle_request(requestMatrix, floor, UP_BUTTON, -1, requestTimeoutMatrix);
@@ -72,6 +56,7 @@ int main(){
 					handle_request(requestMatrix, floor, DOWN_BUTTON, -1, requestTimeoutMatrix);
 				}
 				else{
+					//Om vi timet ut på en opertaor button
 					break;
 				}
 			}
@@ -79,33 +64,23 @@ int main(){
 
 		//If we have finished a floor
 		if(finishedFloor >= 0){
-			finishedFloorMutex.lock;
 			clear_request(requestMatrix,finishedFloor,1,requestTimeoutMatrix);
 			finishedFloor = -1;
-
-			finishedFloorMutex.unlock;
 		}
 
 	}
 }
 
-void handle_request(const bool requestMatrix[NUM_FLOORS][REQUEST_MATRIX_WIDTH], int floor, int button, int externalCost, time_t requestTimeoutMatrix[N_FLOORS]){
+void handle_request(const bool requestMatrix[NUM_FLOORS][REQUEST_MATRIX_WIDTH], int floor, int button, int externalCost, time_t requestTimeoutMatrix[N_FLOORS], NetworkMessage* networkConnection){
 	requestMatrix[floor][button] = 1;
-	if(button == OPERATOR_BUTTON){
+	if(button == buttonCommand){
 		requestMatrix[floor][IS_RESPONSIBLE] = 1;
 	}
 	else{
 		int cost = calculateCost(requestMatrix, floor, button);
 		if(cost > externalCost){
 			requestTimeoutMatrix[floor] = time(NULL) + cost*TIMEOUT_COST_SCALER;
-
-			msgOut.messageType = messageRequest;
-			msgOut.floor = floor;
-			msgOut.button = button;
-			msgOut.price = cost;
-			msgOut.sendTime = time(NULL);
-			send_message(&msgOut);
-
+			networkConnection.sendMessage(messageRequest, floor, button, cost, time(NULL));
 			requestMatrix[floor][IS_RESPONSIBLE] = 1;
 		}
 		else{
@@ -116,15 +91,12 @@ void handle_request(const bool requestMatrix[NUM_FLOORS][REQUEST_MATRIX_WIDTH], 
 	}
 }
 
-void clear_request(const bool requestMatrix[NUM_FLOORS][REQUEST_MATRIX_WIDTH], int floor, bool handledByThisElevator, time_t requestTimeoutMatrix[N_FLOORS]){
+void clear_request(const bool requestMatrix[NUM_FLOORS][REQUEST_MATRIX_WIDTH], int floor, bool handledByThisElevator, time_t requestTimeoutMatrix[N_FLOORS], NetworkMessage networkConnection){
 	if(handledByThisElevator){
 		for(int button=0; button<N_BUTTONS; ++button){
 			requestMatrix[finishedFloor][button] = 0;
 		}
-
-		msgOut.messageType = messageComplete;
-		msgOut.floor = finishedFloor;
-		msgOut.sendTime = time(NULL);
+		networkMessage.send(messageComplete, finishedFloor, buttonUp, NULL,time(NULL));
 	}
 	else{
 		requestMatrix[finishedFloor][UP_BUTTON] = 0;
